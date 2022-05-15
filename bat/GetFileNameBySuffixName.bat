286d01e20fbd5f4f6d3cbf5a1d4e7a840d7f2180
jing_zhong <49520956+jingguotaiduo@users.noreply.github.com>
2022-05-15 11:45:17 +0800

    Create GetFileNameBySuffixName.bat
    Batch script.

---
+++ bat/GetFileNameBySuffixName.bat
@@ -0,0 +1,78 @@
+@echo off
+if not exist *.jpg (
+echo This directory contains no text files.
+) else (
+   echo ********This directory contains the following jpg files********
+   echo.
+   dir /b *.jpg
+   )
+if not exist *.json (
+echo This directory contains no text files.
+) else (
+   echo ********This directory contains the following json files********
+   echo.
+   dir /b *.json
+   )
+if not exist *.md (
+echo This directory contains no text files.
+) else (
+   echo ********This directory contains the following md files********
+   echo.
+   dir /b *.md
+   )
+if not exist *.pdf (
+echo This directory contains no text files.
+) else (
+   echo ********This directory contains the following pdf files********
+   echo.
+   dir /b *.pdf
+   )
+if not exist *.xlsx (
+echo This directory contains no text files.
+) else (
+   echo ********This directory contains the following xlsx files********
+   echo.
+   dir /b *.xlsx
+   )
+if not exist *.pptx (
+echo This directory contains no text files.
+) else (
+   echo ********This directory contains the following pptx files********
+   echo.
+   dir /b *.pptx
+   )
+if not exist *.vsdx (
+echo This directory contains no text files.
+) else (
+   echo ********This directory contains the following vsdx files********
+   echo.
+   dir /b *.vsdx
+   )
+if not exist *.docx (
+echo This directory contains no text files.
+) else (
+   echo ********This directory contains the following docx files********
+   echo.
+   dir /b *.docx
+   )
+if not exist *.mp4 (
+echo This directory contains no text files.
+) else (
+   echo ********This directory contains the following mp4 files********
+   echo.
+   dir /b *.mp4
+   )
+if not exist *.bat (
+echo This directory contains no text files.
+) else (
+   echo ********This directory contains the following bat files********
+   echo.
+   dir /b *.bat
+   )
+if not exist *.txt (
+echo This directory contains no text files.
+) else (
+   echo ********This directory contains the following text files********
+   echo.
+   dir /b *.txt
+   )
